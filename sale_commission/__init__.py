1509388760913f782e52e557a5b06409c0767b1d
Pete Snyder <cybernexus00@yahoo.com>
2024-01-18 14:14:19 -0500

    [UPD] sale_commission: updating module to latest version

--- sale_commission/__init__.py
+++ sale_commission/__init__.py
@@ -1,1 +1,12 @@
 from . import models
+
+
+def _pre_init_sale_commission(cr):
+    """ Allow installing sale_commision in databases with large sale.order table (>1M records)
+        - Creating the computed+stored field commission_total and
+          partner_agent_ids is terribly slow with the ORM and leads to "Out of
+          Memory" crashes
+    """
+    cr.execute("""ALTER TABLE "sale_order" ADD COLUMN "commission_total" double precision;""")
+    cr.execute("""UPDATE sale_order
+                     SET commission_total=0;""")
