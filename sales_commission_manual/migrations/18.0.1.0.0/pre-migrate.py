ee9e638fb43714d217b0b9b4a7ada6f1af16fee9
Pete Snyder <cybernexus00@yahoo.com>
2025-02-05 12:57:15 -0500

    [MIG] remove migrations from new modules

--- sales_commission_manual/migrations/18.0.1.0.0/pre-migrate.py
+++
@@ -1,5 +0,0 @@
-from odoo.upgrade import util
-
-def migrate(cr, version):
-
-    util.rename_module(cr, "sale_commission_manual", "sales_commission_manual")
--- sales_commission_salesman/migrations/18.0.1.0.0/pre-migrate.py
+++
@@ -1,5 +0,0 @@
-from odoo.upgrade import util
-
-def migrate(cr, version):
-
-    util.rename_module(cr, "sale_commission_salesman", "sales_commission_salesman")
